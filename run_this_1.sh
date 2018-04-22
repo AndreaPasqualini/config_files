178ce1339aa13451fe559d837712aef9109123ce
Andrea Pasqualini <apsql@users.noreply.github.com>
2018-04-22 14:43:02 +0200

    Increased performance and code readability

--- run_this_1.sh
+++ run_this_1.sh
@@ -11,25 +11,42 @@ apt update && apt upgrade -y
 
 # Installing packages from APT  ===============================================
 echo 'Installing GTK themes'
-apt install -y arc-theme numix-theme bluebird-gtk-theme breeze-gtk-theme greybird-gtk-theme
-
-echo 'Installing icon themes'
-apt install -y faba-icon-theme faenza-icon-theme moka-icon-theme numix-icon-theme papirus-icon-theme suru-icon-theme
-
-echo 'Installing cursor themes'
-apt install -y oxygen-cursor-theme oxygen-cursor-theme-extra
-
-echo 'Installing fonts'
-apt install -y fonts-cantarell fonts-dejavu fonts-f500 fonts-firacode fonts-noto fonts-noto-mono fonts-quattrocento fonts-roboto fonts-roboto-slab fonts-oxygen
-
-echo 'Installing TeX live system'
-apt install -y texlive-base texlive-bibtex-extra texlive-fonts-recommended texlive-fonts-extra texlive-lang-english texlive-lang-italian texlive-latex-base texlive-pictures texlive-science texlive-pstricks
-
-echo 'Installing 7z'
-apt install -y p7zip-full
-
-echo 'Installing MS fonts'
-apt install -y ttf-mscorefonts-installer
+apt install -y \
+arc-theme \
+numix-theme \
+bluebird-gtk-theme \
+breeze-gtk-theme \
+greybird-gtk-theme \
+faba-icon-theme \
+faenza-icon-theme \
+moka-icon-theme \
+numix-icon-theme \
+papirus-icon-theme \
+suru-icon-theme \
+oxygen-cursor-theme \
+oxygen-cursor-theme-extra \
+fonts-cantarell \
+fonts-dejavu \
+fonts-f500 \
+fonts-firacode \
+fonts-noto \
+fonts-noto-mono \
+fonts-quattrocento \
+fonts-roboto \
+fonts-roboto-slab \
+fonts-oxygen \
+texlive-base \
+texlive-bibtex-extra \
+texlive-fonts-recommended \
+texlive-fonts-extra \
+texlive-lang-english \
+texlive-lang-italian \
+texlive-latex-base \
+texlive-pictures \
+texlive-science \
+texlive-pstricks \
+p7zip-full \
+ttf-mscorefonts-installer
 
 
 # Downloading and installing stuff from the internet  =========================
@@ -43,23 +60,27 @@ dpkg -i google-chrome-stable_current_amd64.deb
 apt-get -f install
 
 echo 'Installing Sublime Text 3'
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg |
+apt-key add -
 apt install apt-transport-https
-echo "deb https://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.list.d/sublime-text.list
+echo "deb https://download.sublimetext.com/ apt/stable/" |
+tee /etc/apt/sources.list.d/sublime-text.list
 apt update && apt install sublime-text
 
 
 # Installing the Conda system  ================================================
-wget -qO - https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-sh ./Miniconda3-latest-Linux-x86_64.sh
+wget -qO - https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh |
+sh
 
 
 # Installing fonts found on the web  ==========================================
 echo 'Installing the Source Code Pro TTF files to /usr/share/fonts/SourceCodePro/'
-wget -qO - https://github.com/adobe-fonts/source-code-pro/archive/2.030R-ro/1.050R-it.zip | 7z x
+wget -qO - https://github.com/adobe-fonts/source-code-pro/archive/2.030R-ro/1.050R-it.zip |
+7z x
 
 echo 'Installing the Fira TTF files to /usr/share/fonts/Fira/'
-wget -qO - https://github.com/mozilla/Fira/archive/4.202.zip | 7z x
+wget -qO - https://github.com/mozilla/Fira/archive/4.202.zip |
+7z x
 
 echo 'Installing the Roboto Mono TTF files to /usr/share/fonts/RobotoMono/'
 mkdir ./RobotoMono/
