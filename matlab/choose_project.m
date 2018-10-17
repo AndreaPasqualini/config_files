7082453950eb21f10d2af7ad3dc9b73c045fd9b3
apsql <andpsq@gmail.com>
2018-10-17 17:26:45 +0200

    Added project chooser for MATLAB

---
+++ matlab/choose_project.m
@@ -0,0 +1,44 @@
+function varargout = choose_project(projectName)
+  % Sets the working directory and adds folders to session path depending on
+  % what project is chosen every time.
+  %
+  %
+  % PARAMETERS
+  % ----------
+  % projectName : str
+  %               A string with the name of the project. If the project is not
+  %               recognized, an exception is raised and nothing will be
+  %               executed. In such case, maybe you want to add the case to this
+  %               function.
+  %
+  %
+  % RETURNS
+  % -------
+  % projectPath : str
+  %               A string returning the root folder of the project.
+  %
+
+  if ~ ischar(projectName)
+    error('''projectName is expected to be a string.')
+  end
+  
+  homePath = 'C:/Users/Andrea';
+  
+  switch projectName
+
+    case 'frame'  % ====================================================== FRAME
+      projectPath = [homePath, '/Documents/frame_wp5'];
+      cd(projectPath)
+      addpath('./code')
+      addpath(genpath('./code/lib'))
+
+    otherwise  % ===================================================== (unknown)
+      error('Unknown project ''%s'': bad input or new project?', projectName)
+
+  end
+  
+  if nargout >= 1
+    varargout{1} = projectPath;
+  end
+
+end
