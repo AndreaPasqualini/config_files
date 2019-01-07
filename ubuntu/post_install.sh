d423a0e54344c074becce6b885d0aca00e0f8bc8
Andrea Pasqualini <apsql@users.noreply.github.com>
2019-01-07 18:31:53 +0100

    Removed echo-ed messages (useless anyways, terminal shows output of operations)

--- ubuntu/post_install.sh
+++ ubuntu/post_install.sh
@@ -1,25 +1,20 @@
 #!/bin/bash
 
 # Update and Upgrade  =========================================================
-echo "Updating and Upgrading"
 sudo apt update && sudo apt upgrade -y
 
 
 # Install Communitheme from Snap store  =======================================
-echo 'Installing Communitheme'
 sudo snap install communitheme
 
 
 # Installing packages from APT  ===============================================
-echo 'Installing general utilities & tools'
 sudo apt install -y \
 git \
 build-essential \
 gdebi \
 synaptic
 
-
-echo 'Installing theming packages via APT'
 sudo apt install -y \
 breeze-gtk-theme \
 materia-gtk-theme \
@@ -27,7 +22,6 @@ papirus-icon-theme \
 qt5-style-plugins \
 qt5ct
 
-echo 'Installing fonts via APT'
 sudo apt install -y \
 fonts-firacode \
 fonts-noto-mono \
@@ -38,7 +32,6 @@ fonts-roboto \
 fonts-roboto-slab \
 ttf-mscorefonts-installer
 
-echo 'Installing TeXlive packages via APT'
 sudo apt install -y \
 texlive-base \
 texlive-bibtex-extra \
@@ -52,7 +45,6 @@ texlive-science \
 texlive-pstricks \
 texstudio
 
-echo 'Installing Python3 packages via APT'
 sudo apt install -y \
 python3-numpy \
 python3-scipy \
@@ -72,25 +64,20 @@ echo "export QT_QPA_PLATFORMTHEME=qt5ct" | sudo tee -a /etc/environment
 
 
 # Installing .deb files from the internet  ====================================
-echo 'Installing Spotify'
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt update && sudo apt install spotify-client
 
 
 # Downloading DEB files  ======================================================
-echo 'Downloading Telegram'
 wget -qP ~/Downloads https://telegram.org/dl/desktop/linux --show-progress
 
-echo 'Downloading Visual Studio Code'
 wget -qP ~/Downloads https://go.microsoft.com/fwlink/?LinkID=760868 --show-progress
 
-echo 'Downloading Steam'
 wget -qP ~/Downloads https://steamcdn-a.akamaihd.net/client/installer/steam.deb --show-progress
 
 
 # Downloading fonts found on the web  =========================================
-echo 'Downloading additional fonts'
 mkdir ~/Downloads/Fira
 mkdir ~/Downloads/RobotoMono
 
@@ -110,7 +97,6 @@ wget -qP ~/Downloads/RobotoMono/ https://github.com/google/fonts/tree/master/apa
 
 # Extracting ZIPs and putting fonts to /usr/share/fonts/truetype/  ============
 cd ~/Downloads
-echo 'Installing downloaded fonts to /usr/share/fonts/truetype/'
 unzip -jq fira.zip ./Fira/
 
 sudo mv ./SourceCodePro /usr/share/fonts/truetype/SourceCodePro/
