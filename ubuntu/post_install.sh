050ca7ea25415a05c797ffc8f03e6bca5610cb12
Andrea Pasqualini <apsql@users.noreply.github.com>
2019-01-08 02:10:04 +0100

    Fixed mistake with value formatting

--- ubuntu/post_install.sh
+++ ubuntu/post_install.sh
@@ -169,12 +169,12 @@ gsettings set org.gnome.desktop.wm.preferences button-layout 'close,minimize,max
 gsettings set org.gnome.desktop.wm.preferences resize-with-right-button true
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left []
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right []
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-up ['<Super>Page_Up']
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-down ['<Super>Page_Down']
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-up "['<Super>Page_Up']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-down "['<Super>Page_Down']"
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-left []
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-right []
-gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-up ['<Super><Shift>Page_Up']
-gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-down ['<Super><Shift>Page_Down']
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-up "['<Super><Shift>Page_Up']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-down "['<Super><Shift>Page_Down']"
 
 
 # Remove all downloaded files upon successful completion of the operations above
