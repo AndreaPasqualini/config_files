1c060cc81ad15c6ad26cc984228c1492d59eb73f
Andrea Pasqualini <apsql@users.noreply.github.com>
2019-01-08 15:20:36 +0100

    Added customization of Shells' top bar to fit my laptop screen

--- ubuntu/tetra_installer.sh
+++ ubuntu/tetra_installer.sh
@@ -11,6 +11,14 @@ sudo ./tetra-gtk-theme/install.sh
 sudo apt purge -y sassc bc
 rm -rf ./tetra-gtk-theme
 
+# Fixing height and font size in Gnome shell top bar for my laptop's screen
+sudo sed -i.bak s/'  height: 28px'/'  height: 40px'/g /usr/share/themes/Tetra/gnome-shell/gnome-shell.css
+sudo sed -i.bak s/'  height: 28px'/'  height: 40px'/g /usr/share/themes/Tetra-dark/gnome-shell/gnome-shell.css
+sudo sed -i.bak s/'  height: 28px'/'  height: 40px'/g /usr/share/themes/Tetra-light/gnome-shell/gnome-shell.css
+sudo sed -i.bak s/'  font-size: 9pt'/'  font-size: 11pt' /usr/share/themes/Tetra/gnome-shell/gnome-shell.css
+sudo sed -i.bak s/'  font-size: 9pt'/'  font-size: 11pt' /usr/share/themes/Tetra-dark/gnome-shell/gnome-shell.css
+sudo sed -i.bak s/'  font-size: 9pt'/'  font-size: 11pt' /usr/share/themes/Tetra-light/gnome-shell/gnome-shell.css
+
 gsettings set org.gnome.desktop.interface gtk-theme 'Tetra'
 gsettings set org.gnome.desktop.interface icon-theme 'ePapirus'
 gsettings set org.gnome.desktop.interface font-name 'Noto Sans 11'
