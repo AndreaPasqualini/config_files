f36e31765c1d8b0d4594bf05323abb4deb5330ab
Andrea Pasqualini <apsql@users.noreply.github.com>
2019-02-05 11:33:24 +0100

    Slimmed down configuration for KDE to avoid redundancy of code

---
+++ ubuntu/kde_config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt install -y \
+muon \
+usb-creator-kde \
+filelight \
+latte-dock
+
+# See https://userbase.kde.org/KDE_System_Administration/Configuration_Files
+# for documentation on the following
