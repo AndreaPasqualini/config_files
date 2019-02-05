ff2de85281f6197999efa93fcc923720c55c5f08
Andrea Pasqualini <apsql@users.noreply.github.com>
2019-02-05 11:32:54 +0100

    Dumped all Gnome-specific instructions into dedicated file

--- ubuntu/post_install.sh
+++ ubuntu/post_install.sh
@@ -4,41 +4,9 @@
 sudo apt update && sudo apt upgrade -y
 
 
-# Install Communitheme from Snap store
-sudo snap install communitheme
-
-
-# Fixing unwanted snaps and installing Tweak Tool
-sudo snap remove \
-gnome-system-monitor \
-gnome-logs \
-gnome-calculator \
-gnome-characters
-
-sudo apt install -y \
-baobab \
-vlc \
-simple-scan \
-gnome-system-monitor \
-gnome-logs \
-gnome-calculator \
-gnome-characters \
-gnome-sudoku \
-gnome-mines \
-gnome-weather \
-chrome-gnome-shell \
-gnome-tweaks
-
-
 # Installing packages from APT
 sudo apt install -y \
 git \
-gdebi \
-synaptic \
-usb-creator-gtk
-
-sudo apt install -y \
-qt5-style-plugins
 build-essential
 
 sudo apt install -y \
@@ -77,10 +45,6 @@ jupyter-notebook \
 spyder3
 
 sudo apt install -y npm
-
-
-# Fix QT5 widget theme in applications
-echo "export QT_QPA_PLATFORMTHEME=gtk2" | sudo tee -a /etc/environment
 
 
 # Installing Spotify
@@ -153,50 +117,6 @@ sudo chown -R root:root /usr/share/fonts/truetype/Fira/
 sudo chown -R root:root /usr/share/fonts/truetype/RobotoMono/
 sudo chown -R root:root /usr/share/fonts/truetype/Slate
 sudo fc-cache -v
-
-
-# Modifying general Gnome settings
-gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize-or-overview'
-gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 36
-gsettings set org.gnome.desktop.background show-desktop-icons false
-gsettings set org.gnome.desktop.background picture-uri 'file:///usr/share/backgrounds/linuxmint-tessa/bfantinatti_small_leaves.jpg'
-gsettings set org.gnome.desktop.interface text-scaling-factor 1.2
-gsettings set org.gnome.desktop.interface clock-show-date true
-gsettings set org.gnome.desktop.interface cursor-size 32
-gsettings set org.gnome.desktop.wm.preferences button-layout 'close,minimize,maximize:'
-gsettings set org.gnome.desktop.wm.preferences resize-with-right-button true
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left []
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right []
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-up "['<Super>Page_Up']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-down "['<Super>Page_Down']"
-gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-left []
-gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-right []
-gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-up "['<Super><Shift>Page_Up']"
-gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-down "['<Super><Shift>Page_Down']"
-gsettings set org.gnome.settings-daemon.plugins.media-keys terminal "'<Super>T'"
-
-
-# Modfying Nautilus settings
-gsettings set org.gnome.nautilus.preferences default-folder-viewer "'list-view'"
-gsettings set org.gnome.nautilus.preferences search-view "'list-view'"
-gsettings set org.gnome.nautilus.preferences search-filter-time-type "'last_modified'"
-gsettings set org.gnome.nautilus.preferences default-visible-columns "['name', 'size', 'type', 'date_modified']"
-
-
-# Modifying Terminal settings
-gsettings set org.gnome.terminal.legacy theme-variant "'dark'"
-
-
-# Modifying Gedit settings
-gsettings set org.gnome.gedit.preferences.editor bracket-matching true
-gsettings set org.gnome.gedit.preferences.editor display-line-numbers true
-gsettings set org.gnome.gedit.preferences.editor display-overview-map true
-gsettings set org.gnome.gedit.preferences.editor display-right-margin true
-gsettings set org.gnome.gedit.preferences.editor highlight-current-line true
-gsettings set org.gnome.gedit.preferences.editor insert-spaces true
-gsettings set org.gnome.gedit.preferences.editor scheme "'tango'"
-gsettings set org.gnome.gedit.preferences.editor tabs-size 4
-gsettings set org.gnome.gedit.preferences.editor wrap-last-split-mode "'word'"
 
 
 # Remove all downloaded files upon successful completion of the operations above
