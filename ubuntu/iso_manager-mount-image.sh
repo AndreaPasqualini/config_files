43a34a67a947314d2066d48b24936aa674977ec0
Andrea Pasqualini <apsql@users.noreply.github.com>
2019-02-18 22:36:53 +0000

    Added aux files for KDE configuration

---
+++ ubuntu/iso_manager-mount-image.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#################################################################
+# For KDE-Services. 2012-2017.                  #
+# By Geovani Barzaga Rodriguez <igeo.cu@gmail.com>      #
+# Improved by Victor Guardiola (vguardiola) Jan 5 2014      #
+#   -Fixed the problem of [dir|file]name with whitespaces.  #
+#################################################################
+
+PATH=/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin:/home/$USER/bin
+MOUNTEXIT=""
+
+##############################
+############ Main ############
+##############################
+
+cd "${1%/*}"
+
+if [ "$1" == "${1%.*}.iso" ]; then
+   fuseiso -p "$1" "${1%.iso}"
+   MOUNTEXIT=$?
+else
+   rename .ISO .iso *
+   kdialog --icon=ks-error --title="Mount ISO-9660 Image" \
+                   --passivepopup="[Error] Can't mount ${1##*/}: Renamed extension of ISO image, because contain uppercase characters. Please try again."
+   exit 1
+fi
+
+if [ "$MOUNTEXIT" = "0" ]; then
+   kdialog --icon=ks-media-optical-mount --title="Mount ISO-9660 Image" --passivepopup="[Finished] ${1##*/} mounted."
+else
+   kdialog --icon=ks-error --title="Mount ISO-9660 Image" \
+                   --passivepopup="[Error] Can't mount ${1##*/}: Already mount or check image integrity."
+   exit 1
+fi
+
+exit 0
--- ubuntu/kde_config.sh
+++ ubuntu/kde_config.sh
@@ -9,3 +9,9 @@ latte-dock
 
 # See https://userbase.kde.org/KDE_System_Administration/Configuration_Files
 # for documentation on the following
+
+# See https://unix.stackexchange.com/questions/47080/mounting-iso-in-linux-kde/319018#319018
+# for reference to the following
+
+sudo mv ./iso_manager-mount-image.sh /bin/iso_manager-mount-image.sh
+mv mount-iso.desktop ~/.local/share/kservices5/ServiceMenus/mount-iso.sh
