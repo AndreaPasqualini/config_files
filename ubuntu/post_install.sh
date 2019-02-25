a3f1fa240630e7c6fe7f6097dab267eb0d40e17c
Andrea Pasqualini <apsql@users.noreply.github.com>
2019-02-25 14:54:49 +0100

    Reduced dimensions of font and upper panel size

--- ubuntu/post_install.sh
+++ ubuntu/post_install.sh
@@ -207,9 +207,9 @@ sudo apt autoremove -y
 rm -rf ./tetra-gtk-theme
 
 # Fixing height and font size in Gnome shell top bar for my laptop's screen
-sudo sed -i.bak s/'  height: 28px'/'  height: 40px'/g /usr/share/themes/Tetra/gnome-shell/gnome-shell.css
-sudo sed -i.bak s/'  height: 28px'/'  height: 40px'/g /usr/share/themes/Tetra-dark/gnome-shell/gnome-shell.css
-sudo sed -i.bak s/'  height: 28px'/'  height: 40px'/g /usr/share/themes/Tetra-light/gnome-shell/gnome-shell.css
+sudo sed -i.bak s/'  height: 28px'/'  height: 36px'/g /usr/share/themes/Tetra/gnome-shell/gnome-shell.css
+sudo sed -i.bak s/'  height: 28px'/'  height: 36px'/g /usr/share/themes/Tetra-dark/gnome-shell/gnome-shell.css
+sudo sed -i.bak s/'  height: 28px'/'  height: 36px'/g /usr/share/themes/Tetra-light/gnome-shell/gnome-shell.css
 sudo sed -i.bak s/'  font-size: 9pt'/'  font-size: 11pt'/ /usr/share/themes/Tetra/gnome-shell/gnome-shell.css
 sudo sed -i.bak s/'  font-size: 9pt'/'  font-size: 11pt'/ /usr/share/themes/Tetra-dark/gnome-shell/gnome-shell.css
 sudo sed -i.bak s/'  font-size: 9pt'/'  font-size: 11pt'/ /usr/share/themes/Tetra-light/gnome-shell/gnome-shell.css
@@ -217,10 +217,10 @@ sudo sed -i.bak s/'  font-size: 9pt'/'  font-size: 11pt'/ /usr/share/themes/Tetr
 # Setting the theme
 gsettings set org.gnome.desktop.interface gtk-theme 'Tetra'
 gsettings set org.gnome.desktop.interface icon-theme 'ePapirus'
-gsettings set org.gnome.desktop.interface font-name 'Noto Sans 11'
-gsettings set org.gnome.desktop.interface monospace-font-name 'Hack 11'
-gsettings set org.gnome.desktop.interface document-font-name 'Noto Sans 11'
-gsettings set org.gnome.desktop.wm.preferences titlebar-font 'Noto Sans Bold 11'
+gsettings set org.gnome.desktop.interface font-name 'Noto Sans 10'
+gsettings set org.gnome.desktop.interface monospace-font-name 'Hack 10'
+gsettings set org.gnome.desktop.interface document-font-name 'Noto Sans 10'
+gsettings set org.gnome.desktop.wm.preferences titlebar-font 'Noto Sans Bold 10'
 
 
 # Remove all downloaded files upon successful completion of the operations above
