a4da2ed898961cb8e1c233b2a0bf26a31ec76814
Andrea Pasqualini <apsql@users.noreply.github.com>
2019-02-27 00:24:49 +0100

    Added configuration files for Linux Mint

---
+++ mint/post_install.sh
@@ -0,0 +1,129 @@
+#!/bin/bash
+
+sudo apt update && sudo apt upgrade -y
+
+sudo apt purge -y \
+thunderbird \
+hexchat \
+rhythmbox
+
+sudo apt install -y \
+fonts-firacode \
+fonts-noto-mono \
+fonts-hack \
+fonts-dejavu \
+fonts-noto \
+fonts-roboto \
+fonts-roboto-slab
+
+sudo apt install -y \
+texlive-base \
+texlive-bibtex-extra \
+texlive-fonts-recommended \
+texlive-fonts-extra \
+texlive-lang-english \
+texlive-lang-italian \
+texlive-latex-base \
+texlive-pictures \
+texlive-science \
+texlive-pstricks \
+texstudio
+
+sudo apt install -y \
+python3-numpy \
+python3-scipy \
+python3-matplotlib \
+python3-sympy \
+python3-pandas \
+python3-numba \
+python3-xlrd \
+ipython3 \
+jupyter \
+jupyter-notebook \
+spyder3
+
+sudo apt install -y \
+spotify-client \
+skypeforlinux \
+telegram-desktop \
+nemo-dropbox \
+sqlitebrowser
+
+wget -q https://mega.nz/linux/MEGAsync/xUbuntu_18.04/amd64/megasync-xUbuntu_18.04_amd64.deb -O ~/Downloads/mega.deb --show-progress
+wget -q https://mega.nz/linux/MEGAsync/xUbuntu_18.04/amd64/nautilus-megasync-xUbuntu_18.04_amd64.deb -O ~/Downloads/mega_nautilus_ext.deb --show-progress
+wget -q https://go.microsoft.com/fwlink/?LinkID=760868 -O ~/Downloads/vscode.deb --show-progress
+wget -q https://github.com/shiftkey/desktop/releases/download/release-1.5.1-linux2/GitHubDesktop-linux-1.5.1-linux2.deb -O ~/Downloads/githubdesktop.deb --show-progress
+wget -q https://download.teamviewer.com/download/linux/teamviewer_amd64.deb -O ~/Downloads/teamviewer.deb --show-progress
+sudo gdebi -n ~/Downloads/mega.deb
+sudo gdebi -n ~/Downloads/mega_nautilus_ext.deb
+sudo gdebi -n ~/Downloads/vscode.deb
+sudo gdebi -n ~/Downloads/githubdesktop.deb
+sudo gdebi -n ~/Downloads/teamviewer.deb
+
+# Downloading fonts found on the web
+mkdir ~/Downloads/Fira
+mkdir ~/Downloads/RobotoMono
+git clone https://github.com/apsql/slate-for-oneplus.git
+wget -q https://github.com/mozilla/Fira/archive/4.202.zip -O ~/Downloads/fira.zip --show-progress
+wget -qP ~/Downloads/RobotoMono/ https://github.com/google/fonts/raw/master/apache/robotomono/RobotoMono-Bold.ttf --show-progress
+wget -qP ~/Downloads/RobotoMono/ https://github.com/google/fonts/raw/master/apache/robotomono/RobotoMono-BoldItalic.ttf --show-progress
+wget -qP ~/Downloads/RobotoMono/ https://github.com/google/fonts/raw/master/apache/robotomono/RobotoMono-Italic.ttf --show-progress
+wget -qP ~/Downloads/RobotoMono/ https://github.com/google/fonts/raw/master/apache/robotomono/RobotoMono-Light.ttf --show-progress
+wget -qP ~/Downloads/RobotoMono/ https://github.com/google/fonts/raw/master/apache/robotomono/RobotoMono-LightItalic.ttf --show-progress
+wget -qP ~/Downloads/RobotoMono/ https://github.com/google/fonts/raw/master/apache/robotomono/RobotoMono-Medium.ttf --show-progress
+wget -qP ~/Downloads/RobotoMono/ https://github.com/google/fonts/raw/master/apache/robotomono/RobotoMono-MediumItalic.ttf --show-progress
+wget -qP ~/Downloads/RobotoMono/ https://github.com/google/fonts/raw/master/apache/robotomono/RobotoMono-Regular.ttf --show-progress
+wget -qP ~/Downloads/RobotoMono/ https://github.com/google/fonts/raw/master/apache/robotomono/RobotoMono-Thin.ttf --show-progress
+wget -qP ~/Downloads/RobotoMono/ https://github.com/google/fonts/raw/master/apache/robotomono/RobotoMono-ThinItalic.ttf --show-progress
+unzip -jq ~/Downloads/fira.zip ~/Downloads/Fira/
+sudo mv ~/Downloads/Fira/* /usr/share/fonts/truetype/Fira/
+sudo mv ~/Downloads/RobotoMono/* /usr/share/fonts/truetype/RobotoMono/
+sudo mv ~/Downloads/slate-for-oneplus/slate/* /usr/share/fonts/truetype/Slate/
+sudo chown -R root:root /usr/share/fonts/truetype/Fira/
+sudo chown -R root:root /usr/share/fonts/truetype/RobotoMono/
+sudo chown -R root:root /usr/share/fonts/truetype/Slate
+sudo fc-cache -v
+
+# Setting xed preferences
+gsettings set org.x.editor.preferences.editor auto-indent true
+gsettings set org.x.editor.preferences.editor bracket-matching true
+gsettings set org.x.editor.preferences.editor display-line-numbers true
+gsettings set org.x.editor.preferences.editor display-right-margin true
+gsettings set org.x.editor.preferences.editor editor-font "'Noto Mono 11'"
+gsettings set org.x.editor.preferences.editor ensure-trailing-newline true
+gsettings set org.x.editor.preferences.editor highlight-current-line true
+gsettings set org.x.editor.preferences.editor insert-spaces true
+gsettings set org.x.editor.preferences.editor prefer-dark-theme true
+gsettings set org.x.editor.preferences.editor right-margin-position 80
+gsettings set org.x.editor.preferences.editor tabs-size 2
+gsettings set org.x.editor.preferences.ui minimap-visible true
+gsettings set org.x.editor.preferences.ui statusbar-visible true
+
+# Setting Nemo preferences
+gsettings set org.nemo.preferences close-device-view-on-device-eject true
+gsettings set org.nemo.preferences context-menus-show-all-actions false
+gsettings set org.nemo.preferences date-format "'informal'"
+gsettings set org.nemo.preferences default-folder-viewer "'list-view'"
+gsettings set org.nemo.preferences default-sort-order "'type'"
+gsettings set org.nemo.preferences ignore-view-metadata true
+gsettings set org.nemo.preferences show-computer-icon-toolbar true
+gsettings set org.nemo.preferences show-home-icon-toolbar true
+gsettings set org.nemo.preferences show-new-folder-icon-toolbar true
+gsettings set org.nemo.preferences show-open-in-terminal-toolbar true
+gsettings set org.nemo.preferences show-reload-icon-toolbar true
+gsettings set org.nemo.desktop desktop-layout "'false::false'"
+gsettings set org.nemo.desktop font "'Noto Sans 11'"
+gsettings set org.nemo.plugins disabled-actions "['add-desklets.nemo-action', 'send-by-mail.nemo-action']"
+
+# Removing unused dependencies
+sudo apt autoremove -y
+
+# Rebooting for peace
+echo 'Rebooting in 10 seconds... [Ctrl+C to cancel]'
+for i in {10..01}
+do
+	echo "$i"
+	sleep 1
+done
+sudo shutdown -r now
+
