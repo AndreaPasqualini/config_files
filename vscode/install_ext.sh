e4432118b92b79cd4ee0466d205cc79ae10036ce
Andrea Pasqualini <apsql@users.noreply.github.com>
2019-06-20 16:31:07 +0200

    Add Bash script for installing custom extensions

---
+++ vscode/install_ext.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+code --install-extension vscode-icons-team.vscode-icons
+code --install-extension ikuyadeu.r
+code --install-extension ms-python.python
+code --install-extension adammaras.overtype
+code --install-extension james-yu.latex-workshop
+code --install-extension yzhang.markdown-all-in-one
+code --install-extension uloco.theme-bluloco-light
+code --install-extension gimly81.matlab
+code --install-extension affenwiesel.matlab-formatter
+code --install-extension vscode-icons-team.vscode-icons
