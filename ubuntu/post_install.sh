5a778db84e125e865284ec5e56dbdc6ac3fba670
Andrea Pasqualini <apsql@outlook.com>
2019-12-12 16:30:59 +0100

    Add a theme fix for QT apps

--- ubuntu/post_install.sh
+++ ubuntu/post_install.sh
@@ -42,6 +42,11 @@ synaptic \
 usb-creator-gtk
 
 
+# Fix (most) QT apps look
+sudo apt install -y qt5-gtk2-platformtheme
+echo 'QT_QPA_PLATFORMTHEME=gtk2' | sudo tee -a /etc/environment
+
+
 # Install Spotify
 curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
