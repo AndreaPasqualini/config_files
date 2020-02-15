5e4ab289f0b0366c43217f4b4a1f486dd7a42473
Andrea Pasqualini <andpsq@gmail.com>
2020-02-15 14:04:50 +0100

    Remove files for KDE Neon setups (it was a hard decision)

--- neon/install_r_packages.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash 
-
-mkdir ~/.R/x86_64-pc-linux-gnu-library/3.6
-
-R -exec ".libPaths(c('~/.R/x86_64-pc-linux-gnu-library/3.6', .libPaths()))"
-R -exec "install.packages(c('gmm', 'ecm', 'fredr', 'eurostat', 'feather', 'readr', 'tidyverse'))
