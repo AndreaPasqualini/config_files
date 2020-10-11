224d604a4e23f4750b18c6463d17f38e9ad7a563
Andrea Pasqualini <andpsq@gmail.com>
2020-10-11 21:04:02 +0200

    Add files for Ubuntu

---
+++ ubuntu/install-miniconda.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+# Download Miniconda
+wget -qP ~/Downloads https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh --show-progress
+
+# Add Miniconda's binaries to PATH
+echo "# Add Miniconda's binaries to PATH" >> ~/.bashrc
+echo 'export PATH="/home/andrea/.miniconda3/bin:$PATH"' >> ~/.bashrc
+
+# Install Miniconda and remove on exit
+bash ~/Downloads/Miniconda3-latest-Linux-x86_64.sh
+rm ~/Downloads/Miniconda3-latest-Linux-x86_64.sh
+
+# Update Miniconda's binaries and install packages
+/home/andrea/.miniconda3/bin/conda update --all
+/home/andrea/.miniconda3/bin/conda install numpy scipy pandas patsy statsmodels matplotlib seaborn ipython jupyterlab
---
+++ ubuntu/post-install.sh
@@ -0,0 +1,33 @@
+# Remove Snap system
+sudo autoremove -y snapd
+
+# Initial cleanup and update
+sudo apt update
+sudo apt upgrade -y
+
+# Install basic tooling
+sudo apt install -y man-db neofetch tty-clock htop lolcat figlet toilet ranger vim curl wget build-essential git pdftk
+
+# Install GUI utilities
+sudo apt install -y baobab totem gnome-calculator usb-creator-gtk
+
+# Install Spotify
+curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add -
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update && sudo apt-get install spotify-client
+
+# Install LibreOffice
+sudo apt install -y libreoffice-writer libreoffice-calc libreoffice-gtk3
+
+# Install TeXlive system
+sudo apt install -y texlive-base texlive-latex-base texlive-latex-recommended texlive-latex-extra texlive-xetex texlive-bibtex-extra texlive-fonts-recommended texlive-fonts-extra texlive-lang-english texlive-lang-italian texlive-pictures texlive-science texlive-pstricks latexmk dvipng
+
+# Install Ruby system
+sudo apt install -y ruby ruby-dev
+echo '# Install Ruby Gems to ~/.gems' >> ~/.bashrc
+echo 'export GEM_HOME="$HOME/.gems"' >> ~/.bashrc
+echo 'export PATH="$HOME/.gems/bin:$PATH"' >> ~/.bashrc
+
+# Fix (most) QT apps look
+sudo apt install -y qt5-gtk2-platformtheme
+echo 'QT_QPA_PLATFORMTHEME=gtk2' | sudo tee -a /etc/environment
