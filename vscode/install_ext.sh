8fc53897ab8e2b97a4e15f5d4e00033fb0ba0b86
Andrea Pasqualini <andpsq@gmail.com>
2020-10-11 20:55:55 +0200

    Update VSCode extensions

--- vscode/install_ext.sh
+++ vscode/install_ext.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+code --install-extension github.github-vscode-theme
 code --install-extension vscode-icons-team.vscode-icons
-code --install-extension ikuyadeu.r
-code --install-extension ms-python.python
-code --install-extension adammaras.overtype
-code --install-extension james-yu.latex-workshop
+code --install-extension kylebarron.stata-enhanced
+code --install-extension grapecity.gc-excelviewer
 code --install-extension yzhang.markdown-all-in-one
-code --install-extension uloco.theme-bluloco-light
 code --install-extension gimly81.matlab
 code --install-extension affenwiesel.matlab-formatter
-code --install-extension vscode-icons-team.vscode-icons
+code --install-extension adammaras.overtype
+code --install-extension ms-python.python
+code --install-extension ikuyadeu.r
+code --install-extension james-yu.latex-workshop
+code --install-extension yahya-gilany.vscode-clock
