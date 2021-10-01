149d3cf023598880854b62340430a9593a9647c3
AndreaPasqualini <andpsq@gmail.com>
2021-10-01 18:59:18 +0100

    Inform the user to change a setting in Jupyter's config

--- wsl/install-miniconda.sh
+++ wsl/install-miniconda.sh
@@ -14,3 +14,12 @@ rm $HOME/Downloads/Miniconda3-latest-Linux-x86_64.sh
 # Update Miniconda's binaries and install packages
 $HOME/.miniconda3/bin/conda update --all
 $HOME/.miniconda3/bin/conda install numpy scipy pandas patsy statsmodels matplotlib seaborn ipython jupyterlab pylint plotly
+
+# Generate the configuration file for the Jupyter server
+$HOME/.miniconda/bin/jupyter notebook --generate-config
+
+echo ''
+echo 'Go to the location of the configuration file and search for the following'
+echo '    c.NotebookApp.use_redirect_file'
+echo 'It will be commented out. Remove the comment symbol and set it to False.'
+echo "This way, Windows' browser will be correctly launched by the server."
