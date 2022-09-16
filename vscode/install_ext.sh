5bd3078039ded74b64e0234ac88cf06d11494f14
AndreaPasqualini <andpsq@gmail.com>
2022-09-17 00:52:19 +0100

    Add VS Code extensions

--- vscode/install_ext.sh
+++ vscode/install_ext.sh
@@ -2,6 +2,7 @@
 
 code --install-extension tinkertrain.theme-panda
 code --install-extension github.github-vscode-theme
+code --install-extension arcticicestudio.nord-visual-studio-code
 code --install-extension vscode-icons-team.vscode-icons
 code --install-extension grapecity.gc-excelviewer
 code --install-extension yzhang.markdown-all-in-one
@@ -10,3 +11,4 @@ code --install-extension ms-python.python
 code --install-extension reditorsupport.r
 code --install-extension james-yu.latex-workshop
 code --install-extension yahya-gilany.vscode-clock
+code --install-extension ban.spellright
