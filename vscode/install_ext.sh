8bca465730117b57028aa7355cee262ad9afb32b
AndreaPasqualini <andpsq@gmail.com>
2022-09-17 00:51:49 +0100

    Remove extensions for proprietary languages

--- vscode/install_ext.sh
+++ vscode/install_ext.sh
@@ -3,11 +3,8 @@
 code --install-extension tinkertrain.theme-panda
 code --install-extension github.github-vscode-theme
 code --install-extension vscode-icons-team.vscode-icons
-code --install-extension kylebarron.stata-enhanced
 code --install-extension grapecity.gc-excelviewer
 code --install-extension yzhang.markdown-all-in-one
-code --install-extension gimly81.matlab
-code --install-extension affenwiesel.matlab-formatter
 code --install-extension adammaras.overtype
 code --install-extension ms-python.python
 code --install-extension ikuyadeu.r
