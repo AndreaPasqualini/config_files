e843200c89f796d8910af20a884460f78a1c9195
AndreaPasqualini <andpsq@gmail.com>
2022-09-17 00:53:12 +0100

    Streamline winget installs for Windows 11

--- windows/install-setup.ps1
+++ windows/install-setup.ps1
@@ -1,24 +1,20 @@
 winget install --id 7zip.7zip --exact --source winget
 winget install --id Adobe.Acrobat.Reader.64-bit --exact --source winget
-winget install --id Anaconda.Miniconda3 --exact --source winget --interactive
-winget install --id ArtifexSoftware.GhostScript --exact --source winget
-winget install --id ChristianSchenk.MiKTeX --exact --source winget
+winget install --id darktable.darktable --exact --source winget
 winget install --id Dropbox.Dropbox --exact --source winget
-winget install --id GitHub.GitHubDesktop --exact --source winget
+winget install --id 9PFXCD722M2C --exact --source msstore  # Filelight
+winget install --id Inkscape.Inkscape --exact --source winget
+winget install --id IrfanSkiljan.IrfanView --exact --source winget
 winget install --id 9NHL4NSC67WM --exact --source msstore  # JackieLiu.NotepadsApp
 winget install --id Microsoft.PowerToys --exact --source winget
-winget install --id Microsoft.ROpen --exact --source winget
 winget install --id Microsoft.Skype --exact --source winget
 winget install --id Microsoft.Teams --exact --source winget
 winget install --id Microsoft.VisualStudioCode --exact --source winget
-winget install --id 9N0DX20HK701 --exact --source msstore  # Microsoft.WindowsTerminal
 winget install --id PDFLabs.PDFtkServer --exact --source winget
-winget install --id JetBrains.PyCharm.Professional --exact --source winget
-winget install --id RStudio.RStudio.OpenSource --exact --source winget
 winget install --id Spotify.Spotify --exact --source winget
 winget install --id SumatraPDF.SumatraPDF --exact --source winget
+winget install --id Teamviewer.Teamviewer --exact --source winget
 winget install --id Telegram.TelegramDesktop --exact --source winget
-winget install --id TeXstudio.TeXstudio --exact --source winget
 winget install --id Transmission.Transmission --exact --source winget
 winget install --id Valve.Steam --exact --source winget
 winget install --id VideoLAN.VLC --exact --source winget --interactive
