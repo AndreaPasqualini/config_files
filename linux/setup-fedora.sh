7f2725b835068c66d2f48f02ea710f874f2ae38f
AndreaPasqualini <andpsq@gmail.com>
2022-09-30 02:03:31 +0100

    Add comments to explain what's happening

--- linux/setup-fedora.sh
+++ linux/setup-fedora.sh
@@ -2,14 +2,25 @@ mkdir $HOME/Git
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-sudo dnf install neofetch tldr pdftk gnome-tweaks gnome-console powerline vim-powerline darktable inkscape libinput-devel meson ibm-plex-fonts-all cascadia-fonts-all google-noto-fonts-common dconf-editor systemd-devel
+# Install CLI programs
+sudo dnf install neofetch tldr pdftk powerline vim-powerline
 
+# Install GUI programs
+sudo dnf install gnome-console gnome-tweaks dconf-editor solaar darktable inkscape
+
+# Install fonts
+sudo dnf install ibm-plex-fonts-all cascadia-fonts-all google-noto-fonts-common
+
+# Install Gnome extensions
 sudo dnf install gnome-extensions gnome-shell-extension-appindicator gnome-shell-extension-caffeine gnome-shell-extension-just-perfection gnome-shell-extension-native-window-placement
 
+# Install subset of TeXlive distribution
 sudo dnf install texlive-base texlive-collection-basic texlive-collection-binextra texlive-collection-plaingeneric texlive-collection-latex texlive-collection-latexrecommended texlive-collection-latexextra texlive-collection-fontsrecommended texlive-collection-fontsextra texlive-collection-mathscience texlive-collection-pictures texlive-collection-bibtexextra texlive-collection-publishers texlive-collection-langenglish texlive-collection-langitalian
 
-sudo dnf install R-core R-core-devel libcurl-devel openssl-devel
+# Install R
+sudo dnf install R-core R-core-devel libcurl-devel openssl-devel libxml2-devel
 
+# Install VS Code
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 sudo dnf check-update
