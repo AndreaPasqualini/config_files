f61f85efdc771f4c3ed04dbcfb4860c8a6e892bb
AndreaPasqualini <andpsq@gmail.com>
2022-10-01 12:53:34 +0100

    Install btop on WSL

--- linux/setup-wsl.sh
+++ linux/setup-wsl.sh
@@ -47,3 +47,17 @@ echo '' >> $HOME/.bashrc
 
 echo 'Reload the configuration for Bash with "source ~/.bashrc"'
 echo 'Then install Bundler and Jekyll with "gem install bundler jekyll"'
+
+# Install btop
+cd $HOME/Downloads
+wget --quiet --show-progress https://github.com/aristocratos/btop/releases/download/v1.2.9/btop-x86_64-linux-musl.tbz
+mkdir btop
+tar xf ./btop-x86_64-linux-musl.tbz --directory=btop
+cd btop
+sudo make install
+sudo make setuid
+cd ..
+rm ./btop/ -rf
+rm ./btop-x86_64-linux-musl.tbz
+cd $HOME
+
